d50583348fc88afe3e8bc769ce58706acdec1d62
reflection1921 <firesoft1212@gmail.com>
2021-01-16 03:40:26 +0900

    v20210116

--- darktheme.js
+++ darktheme.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KakaoStory Dark Theme
 // @namespace    http://chihaya.kr
-// @version      0.5
+// @version      0.6
 // @description  Make dark theme for KakaoStory
 // @author       Reflection
 // @match        https://story.kakao.com/*
@@ -54,6 +54,7 @@ xmlHttp.onreadystatechange = function() {
        filter = replaceAll(filter, "story.kakao.com#$#", "");
        GM_addStyle ( filter );
        GM_addStyle ( ".ico_ks2 {background: url(\'https://raw.githubusercontent.com/reflection1921/KakaoStory-DarkTheme/master/ico_ks2.png\') no-repeat 0 0; !important;}" );
+       GM_addStyle ( ".ico_ks {background: url(\'https://raw.githubusercontent.com/reflection1921/KakaoStory-DarkTheme/master/ico_ks.png\') no-repeat 0 0; !important;}" );
     }
 }
 xmlHttp.open("GET", "https://raw.githubusercontent.com/reflection1921/KakaoStory-DarkTheme/master/darktheme.txt");
